34bc10205fd901dbd9a71aaa2f968c6cfe0a5787
anietooo <nietsalex@gmail.com>
2024-12-13 13:02:10 +0100

    views implementadas en formularios

---
+++ database/productoDB.php
@@ -0,0 +1,27 @@
+<?php
+require_once("../views/header.php")
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario de productos</title>
+</head>
+<style>
+    body{
+        display: grid;
+        height: 100dvh;
+        grid-template-rows: auto 1fr auto;
+    }
+</style>
+<body>
+    <!--HACER AQUI FORMULARIO-->
+    <form action="">
+        <input type="text" placeholder="hola">
+    </form>
+</body>
+</html>
+<?php
+require_once("../views/footer.php")
+?>
