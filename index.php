b5b1e7f580040228a0cdc876014eaf44cf5dfcbd
anietooo <nietsalex@gmail.com>
2024-12-17 10:25:46 +0100

    index empezado

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+require_once("./views/header.php")
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+      crossorigin="anonymous"
+    ></script>
+    <style>
+        body{
+            display: grid;
+            height: 100dvh;
+            grid-template-rows: auto 1fr auto;
+        }
+    </style>
+</head>
+<body>
+    Esto es Index
+</body>
+</html>
+<?php
+require_once("./views/footer.php")
+?>
